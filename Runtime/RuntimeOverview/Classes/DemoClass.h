1522c7895386513e625f97a8d74569c66a9db8ae
朱献国 <zhuxianguo529@163.com>
2019-05-27 22:30:28 +0800

    add new

--- Runtime/RuntimeOverview/Classes/DemoClass.h
+++ Runtime/RuntimeOverview/Classes/DemoClass.h
@@ -8,6 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ 应用场景：
+ 
+ 方法交换：  1. 可变容器(字典、数组等)的空操作、下标越界的导致程序crash的问题。
+           2. 检测类实例的内存释放 dealloc。
+           3. UITableView 或 UICollectionView 的reloadData方法，添加缺省页面。
+ 类添加方法：
+            1. 解决系统的兼容性的问题。 例如 NSString的 containsString: ios(8.0),
+ */
+
 @interface DemoClass : NSObject <NSObject> {
     NSNumber *_age;
 }
