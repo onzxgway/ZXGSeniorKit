a7cc9e03a4fd2c54be77276823b21741477807aa
朱献国 <zhuxianguo529@163.com>
2019-06-04 09:52:27 +0800

    Update BarrierViewController.m

--- Thread/GCD/Classes/BarrierViewController.m
+++ Thread/GCD/Classes/BarrierViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "BarrierViewController.h"
+#import "Person.h"
 
-@interface BarrierViewController ()
-//{
-//    NSMutableArray *_safeAry;
-//    dispatch_queue_t rwQueue;
-//}
-//@property (nonatomic, strong) NSMutableDictionary *dict;
+@interface BarrierViewController () {
+    NSMutableArray *_safeAry;
+    dispatch_queue_t _queue;
+}
+@property (nonatomic, strong) NSMutableDictionary *dict;
+
 @end
 
 @implementation BarrierViewController
@@ -22,56 +23,51 @@
     [super viewDidLoad];
     self.view.backgroundColor = UIColorRandomColor;
     self.navigationItem.title = @"dispatch_barrier_";
+    
+    _safeAry = [NSMutableArray array];
+    [_safeAry addObject:@"0"];
+    [_safeAry addObject:@"1"];
+    [_safeAry addObject:@"2"];
+    [_safeAry addObject:@"3"];
+    _queue = dispatch_queue_create("concurrentQueue", DISPATCH_QUEUE_CONCURRENT);
 }
 
+#pragma mark - ========== dispatch_barrier_async 应用场景一 ==========
 /**
- - (void)viewDidLoad {
- [super viewDidLoad];
- 
- self.view.backgroundColor = [UIColor lightGrayColor];
- 
- _safeAry = [NSMutableArray array];
- //    [_safeAry addObject:@"0"];
- //    [_safeAry addObject:@"1"];
- //    [_safeAry addObject:@"2"];
- //    [_safeAry addObject:@"3"];
- rwQueue = dispatch_queue_create("concurrentQueue", DISPATCH_QUEUE_CONCURRENT);
- 
- [self apply_Two];
- }
+ 多线程来操作可变数组mutableAry，肯定会出错，因为集合都是线程不安全的。
+ 解决方案：使用dispatch_barrier_async。 只要涉及到写操作（要做保护）
+  */
+- (void)barrier_one {
+    
+    dispatch_queue_t queue = dispatch_queue_create("testRWAry", DISPATCH_QUEUE_CONCURRENT);
+    for (int i = 0; i < 20; i++) {
+        // 写
+        dispatch_async(queue, ^{
+            [self addObject:[NSString stringWithFormat:@"%d", i + 4]];
+        });
 
- 读写数组mutableAry,如果开几个线程来操作数组
- 1 写数组（移除index=0对象）， 2 写数组（移除index=0的对象） ／／ 有问题
- 1 读数组 ， 2 读数组 ／／ 没问题
- 1 读数组    2写数组
- 只要涉及到写操作（要做保护）
- 
-- (void)testRWAry {
+        // 读
+        dispatch_async(queue, ^{
+            NSLog(@"%d::%@", i, [self indexTo:i]);
+        });
+    }
     
-    //    dispatch_queue_t queue = dispatch_queue_create("testRWAry", DISPATCH_QUEUE_CONCURRENT);
-    //    for (int i = 0; i < 20; i++) {
-    //
-    //        // 写
-    //        dispatch_async(queue, ^{
-    ////            [self addObject:[NSString stringWithFormat:@"%d", i + 4]];
-    //            [NSThread sleepForTimeInterval:0.05];
-    //            [self.dict setObject:@(i) forKey:[@(i) stringValue]];
-    //        });
-    //
-    ////        // 读
-    ////        dispatch_async(queue, ^{
-    ////            NSLog(@"%d::%@", i, [self indexTo:i]);
-    ////        });
-    //    }
-    //
-    //    dispatch_barrier_async(queue, ^{
-    ////        for (int i = 0; i < self.dict.count; i++) {
-    ////            NSLog(@"%d::%@", i, [self indexTo:i]);
-    ////        }
-    //
-    //        NSLog(@"%@", self.dict);
-    //    });
+//        dispatch_barrier_async(queue, ^{
+    //        for (int i = 0; i < self.dict.count; i++) {
+    //            NSLog(@"%d::%@", i, [self indexTo:i]);
+    //        }
     
+//            NSLog(@"%@", self.dict);
+//        });
+}
+
+#pragma mark - ========== dispatch_barrier_async 应用场景二 ==========
+/**
+ 多线程来操作可变数组mutableAry，肯定会出错，因为集合都是线程不安全的。
+ 解决方案：使用dispatch_barrier_async。 只要涉及到写操作（要做保护）
+ */
+- (void)barrier_two {
+        
     Person *p = [[Person alloc] init];
     p.queue = dispatch_queue_create("abbb", DISPATCH_QUEUE_CONCURRENT);
     [self updateContact:p contacts:self.dict];
@@ -80,76 +76,73 @@
 
 - (void)updateContact:(Person *)person contacts:(NSDictionary *)contacts {
     
-    //    for (NSInteger i = 0; i < contacts.allKeys.count; ++i) {
-    //        NSString *key = [contacts.allKeys objectAtIndex:i];
-    //        NSString *value = [contacts objectForKey:key];
-    //        dispatch_async(rwQueue, ^{
-    //            [person setProperty:key email:value];
-    //            NSLog(@"%@", person);
-    //        });
-    //    }
+//        for (NSInteger i = 0; i < contacts.allKeys.count; ++i) {
+//            NSString *key = [contacts.allKeys objectAtIndex:i];
+//            NSString *value = [contacts objectForKey:key];
+//            dispatch_async(rwQueue, ^{
+//                [person setProperty:key email:value];
+//                NSLog(@"%@", person);
+//            });
+//        }
     //    dispatch_barrier_async(rwQueue, ^{
     //        NSLog(@"==> Final %@",person);
     //    });
     
-    dispatch_group_t group = dispatch_group_create();
     
+    dispatch_group_t group = dispatch_group_create();
     for (NSInteger i = 0; i < contacts.allKeys.count; ++i) {
         NSString *key = [contacts.allKeys objectAtIndex:i];
         NSString *value = [contacts objectForKey:key];
         
-        dispatch_group_async(group, rwQueue, ^{
+        dispatch_group_async(group, _queue, ^{
             [person setProperty:key email:value];
         });
         
     }
-    
-    dispatch_group_notify(group, rwQueue, ^{
+    dispatch_group_notify(group, _queue, ^{
         NSLog(@"==> Final %@", person);
     });
 }
 
+
+- (NSMutableDictionary *)dict {
+ if (!_dict) {
+     _dict = [@{
+         @"Goodguy" : @"crafttang@gmail.com",
+         @"Leijun" : @"leijun@mi.com",
+         @"Yuchengdong" : @"yuchengdong@huawei.com",
+         @"Luoyonghao" : @"luoyonghao@smartisan.com",
+         } mutableCopy];
+     }
+     return _dict;
+}
+
+
 // 写 保证只有一个在操作（避免了同时多个写操作导致的问题）
 - (void)addObject:(NSString *)object {
     
-    //    dispatch_barrier_async(rwQueue, ^{
-    if (object != nil) {
-        [_safeAry addObject:object];
-    }
-    //    });
-    
+    dispatch_barrier_async(_queue, ^{
+        if (object != nil) {
+            [_safeAry addObject:object];
+        }
+    });
 }
 
 // 主队列中的任务必须由主线程执行
 // 注意同步，因为业务关系，必须马上数据
 - (NSString *)indexTo:(NSInteger)index {
-    //    __block NSString *result = nil;
-    NSString *result = nil;
-    //    dispatch_sync(rwQueue, ^{
-    if (index < _safeAry.count) {
-        result = _safeAry[index];
-    }
-    //    });
+    __block NSString *result = nil;
+    dispatch_sync(_queue, ^{
+        if (index < _safeAry.count) {
+            result = _safeAry[index];
+        }
+    });
     return result;
 }
 
-- (NSMutableDictionary *)dict {
-    
-    if (!_dict) {
-        _dict = [@{
-                   @"Goodguy" : @"crafttang@gmail.com",
-                   @"Leijun" : @"leijun@mi.com",
-                   @"Yuchengdong" : @"yuchengdong@huawei.com",
-                   @"Luoyonghao" : @"luoyonghao@smartisan.com",
-                   } mutableCopy];
-    }
-    return _dict;
-    
-}
- */
-
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    
+//    [self barrier_one];
+    [self barrier_two];
 }
 
 // 隔断方法：当前面的写入操作全部完成之后，再执行后面的读取任务。当然也可以用Dispatch Group和dispatch_set_target_queue,只是比较而言 dispatch_barrier_async 会更加顺滑
