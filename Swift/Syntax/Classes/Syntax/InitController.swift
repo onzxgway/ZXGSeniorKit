8f965cc4ed295db6956462d622ba5b32e0a78237
朱献国 <zhuxianguo529@163.com>
2019-08-12 21:19:22 +0800

    Update InitController.swift

--- Swift/Syntax/Classes/Syntax/InitController.swift
+++ Swift/Syntax/Classes/Syntax/InitController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
+/*
+    构造过程：
+        使用类、结构体或枚举类型实例之前的准备过程。在新实例使用之前这个过程是必须的，它包括设置实例中每个存储属性的初始值 和 执行其它必须的设置或构造过程。
+ 
+    如果实现构造过程：
+            通过定义构造器来实现构造过程。
+    构造器：
+            创建类型新实例的特殊方法。与 Objective-C 中的构造器不同，Swift 的构造器没有返回值。
+ */
+
+/*
+    存储属性的初始赋值
+ 
+        类和结构体在创建实例时，必须为所有存储型属性设置合适的初始值。存储型属性的值不能处于一个未知的状态。
+        赋值时机：
+            1.可以在构造器中为存储型属性设置初始值。
+            2.也可以在定义存储型属性的时候分配默认值。
+        注意：
+        当你为存储型属性分配默认值或者在构造器中为设置初始值时，它们的值是被直接设置的，不会触发任何属性观察者。
+ */
 class InitController: SyntaxBaseController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
